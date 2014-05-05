4df8f4c2308548707c4ee3c49d66f9c60adb67f6
wojtekw92 <wwozniak92@gmail.com>
2014-05-05 23:54:35 +0200

    Gierka

---
+++ js/main.js
@@ -0,0 +1,123 @@
+var c=document.getElementById("gra");
+var ctx=c.getContext("2d");
+//TUTAJ KLASA GRACZA
+function Gamer(x,y,r)
+{
+	this.x=x;
+	this.y=y;
+	this.r=r;
+}
+Gamer.prototype.x=0;
+Gamer.prototype.y=0;
+Gamer.prototype.r=0;
+Gamer.prototype.gravity=function(){
+		if(this.y+this.r<400)this.y+=0.8;
+}
+Gamer.prototype.moveUp=function(){
+	if(this.y>10+this.r)this.y-=25;
+}
+Gamer.prototype.moveDown=function(){
+	if(this.y+this.r+10<400)this.y+=10;
+}
+Gamer.prototype.draw=function(context){
+	context.beginPath();
+	context.arc(this.x,this.y,this.r,0,2*Math.PI);
+	context.fillStyle = "#000000";
+	context.stroke();
+}
+//TUTAJ ZROBIMY KLASĘ DLA PRZESKODY
+function Obstacle(y,gap,x)//constructor
+{
+	this.y=y;
+	this.gap=gap;
+	this.x=x;
+}
+Obstacle.prototype.x=0;
+Obstacle.prototype.y=0;
+Obstacle.prototype.gap=40;
+Obstacle.prototype.width=20;
+Obstacle.prototype.chceckColision= function(obiekt)
+{
+	if(this.x+this.width>obiekt.x && obiekt.x>this.x)
+	{
+		if(obiekt.y-obiekt.r>this.y && obiekt.y+obiekt.r<this.y+this.gap) return false;
+		else return true;
+	}
+	else return false;
+}
+Obstacle.prototype.draw= function(context)
+{
+	context.fillStyle = "#FF0000";
+	context.fillRect(this.x,this.y+this.gap,this.width,400-this.y-this.gap);
+	context.fillRect(this.x,0,this.width,this.y);
+}
+Obstacle.prototype.moveTo=function(moveToX){
+	this.x=moveToX;
+}
+Obstacle.prototype.moveLeft=function(){
+	if(this.x>-20)this.x--;
+}
+Obstacle.prototype.visible=function(){
+	if(this.x>-20 && this.x<600) return true;
+	else return false;
+}
+//=======================================================
+
+
+
+
+
+var points=0;
+document.onkeydown = checkKey;
+c.onclick=function(){
+gracz.moveUp();
+}
+var imageObj = new Image();
+imageObj.src = 'img/bg.jpg';
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '38') {        // up arrow
+		gracz.moveUp();
+    }
+    else if (e.keyCode == '40') {
+	   gracz.moveDown();
+    }
+}
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var gracz=new Gamer(300,200,5);
+var przeszkody= [];
+przeszkody.push(new Obstacle(getRandomInt(80, 320),getRandomInt(60, 100),600));
+function menu()
+{
+	ctx.clearRect(0,0,600,400);//wyczysc ekran
+	ctx.drawImage(imageObj, 0, 0, 600, 400);//narysuj tło
+	ctx.font = "40px Arial Black";
+	ctx.fillStyle = "#000000";
+	ctx.fillText("FLOPPY BALL",100,50);
+}
+function mainLoop()
+{
+	ctx.clearRect(0,0,600,400);//wyczysc ekran
+	//ctx.drawImage(imageObj, 0, 0, 600, 400);//narysuj tło
+	for(var i=0;i<przeszkody.length;i++) przeszkody[i].draw(ctx);
+	points+=0.1;
+	ctx.font = "20px Arial";
+	ctx.strokeText("POINTS: "+Math.floor(points),10,50);
+	if(przeszkody.length>1 && !przeszkody[0].visible())przeszkody.shift();
+	if(przeszkody[przeszkody.length-1].visible()) przeszkody.push(new Obstacle(getRandomInt(80, 320),getRandomInt(60, 100),600+getRandomInt(150, 300)));
+	gracz.gravity();
+	gracz.draw(ctx);
+	for(var i=0;i<przeszkody.length;i++){
+		if(przeszkody[i].chceckColision(gracz))
+		{
+			clearInterval(myVar);
+			alert("game over!");
+		}
+		przeszkody[i].moveLeft();
+	}
+}
+//menu();
+var myVar=setInterval(mainLoop,10);
+		
